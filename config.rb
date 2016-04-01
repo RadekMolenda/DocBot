56a6d8d42625effad056529d7647389a87a434b3
Radek Molenda <rmolenda@leadformance.com>
2016-04-01 21:21:13 +0200

    Nicer fail syntax

--- config.rb
+++ config.rb
@@ -3,7 +3,10 @@
 require "slack-ruby-client"
 require "celluloid"
 
+def config_fail
+  raise "Missing ENV[SLACK_API_TOKEN]!"
+end
+
 Slack.configure do |config|
-  config.token = ENV["SLACK_API_TOKEN"]
-  raise "Missing ENV[SLACK_API_TOKEN]!" unless config.token
+  config.token = ENV["SLACK_API_TOKEN"] || config_fail
 end
